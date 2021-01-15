53c6dec97cd7c43d7f6f325ef78d0ee48ce76b9c
David Poslušný <david.poslusny@email.cz>
2021-01-15 21:35:05 +0100

    První práce na grafickém rozhraní a přidání ikonky.

--- src/main/java/cz/vse/restaurace/main/Start.java
+++ src/main/java/cz/vse/restaurace/main/Start.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.InputStream;
@@ -15,14 +16,18 @@ public class Start extends Application {
     }
 
     public void start(Stage stage) throws Exception {
-        //stage.setMaximized(true);
 
         FXMLLoader loader = new FXMLLoader();
-        InputStream stream = getClass().getClassLoader().getResourceAsStream("screen/scene_main.fxml");
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("scene_login.fxml");
         Parent root = loader.load(stream);
 
         Scene scene = new Scene(root);
         stage.setScene(scene);
+
+        //Ikona
+        InputStream streamIcon = getClass().getClassLoader().getResourceAsStream("img/reserved.png");
+        Image imageIcon = new Image(streamIcon);
+        stage.getIcons().add(imageIcon);
 
         stage.show();
     }
