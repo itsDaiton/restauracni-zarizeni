f4d7a33f386390e03baf69e9c16ec3e3d905fd76
David Poslušný <david.poslusny@email.cz>
2021-01-15 17:42:29 +0100

    Upravení třídy Start - nyní lze spouštet okna.

--- src/main/java/cz/vse/restaurace/main/Start.java
+++ src/main/java/cz/vse/restaurace/main/Start.java
@@ -1,4 +1,29 @@
 package cz.vse.restaurace.main;
 
-public class Start {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.InputStream;
+
+public class Start extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.setMaximized(true);
+
+        FXMLLoader loader = new FXMLLoader();
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("screen/scene_main.fxml");
+        Parent root = loader.load(stream);
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+
+        stage.show();
+    }
 }
